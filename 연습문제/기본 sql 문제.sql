3f4fd49f5fa2e3766ea46ed8b6d6f46e7ee8d1b6
sinhu-jung <tlsgn26@naver.com>
2021-05-17 16:53:22 +0900

    기본 문제

--- 연습문제/기본 sql 문제.sql
+++ 연습문제/기본 sql 문제.sql
@@ -9,3 +9,25 @@ select concat(first_name, ' ' , last_name) as 이름, gender as 성별, hire_dat
 select gender, count(gender) from employees group by gender;
 
 -- 4. 현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.)
+select count(emp_no) from salaries where to_date = '9999-01-01';
+
+-- 5. 부서는 총 몇 개가 있나요?
+select count(dept_name) from departments;
+
+-- 6. 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
+select count(emp_no) from dept_manager where to_date = '9999-01-01';
+
+-- 7. 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
+select dept_name from departments order by length(dept_name) desc;
+
+-- 8. 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
+select count(emp_no) from salaries where to_date = '9999-01-01'; 
+
+-- 9. 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
+select distinct title from titles order by length(title) desc;
+
+-- 10. 현재 Enginner 직책의 사원은 총 몇 명입니까?
+select count(emp_no) from titles where title = 'Engineer';
+
+-- 11. 사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
+select emp_no, title, from_date, to_date from titles where emp_no = '13250' order by from_date;
