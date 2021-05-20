a12e5b543476fea21ac5fbeedf7a8fef691be2e8
sinhu-jung <tlsgn26@naver.com>
2021-05-20 16:06:11 +0900

    BookShop #1

---
+++ jdbc-practices/src/main/java/bookshop/vo/BookVo.java
@@ -0,0 +1,46 @@
+package bookshop.vo;
+
+public class BookVo {
+	private Long no;
+	private String title;
+	private String status;
+	private String authorName;
+	private Long authorNo;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public Long getAuthorNo() {
+		return authorNo;
+	}
+	public void setAuthorNo(Long authorNo) {
+		this.authorNo = authorNo;
+	}
+	@Override
+	public String toString() {
+		return "BookVo [no=" + no + ", title=" + title + ", status=" + status + ", authorName=" + authorName
+				+ ", authorNo=" + authorNo + "]";
+	}
+	
+	
+}
---
+++ sql연습/bookshop.sql
@@ -0,0 +1,2 @@
+desc author;
+desc book;
